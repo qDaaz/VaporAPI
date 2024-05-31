afb8191ccf516733e7d370af0fc16be3657531da
santiagobeiro <69767529+qDaaz@users.noreply.github.com>
2024-05-31 19:48:42 -0300

    Added is(Faction) Methods

--- src/main/java/dev/astro/net/VaporAPI.java
+++ src/main/java/dev/astro/net/VaporAPI.java
@@ -3,6 +3,8 @@ package dev.astro.net;
 import dev.astro.net.deathban.DeathbanManager;
 import dev.astro.net.timer.TimerManager;
 import dev.astro.net.user.ProfileManager;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.IllegalPluginAccessException;
 
 import java.util.UUID;
@@ -18,6 +20,38 @@ public class VaporAPI {
     }
 
     public DeathbanManager getDeathban(UUID uuid) {
+        throw new IllegalPluginAccessException("VaporAPI not found");
+    }
+
+    public boolean isSpawn(Location location) {
+        throw new IllegalPluginAccessException("VaporAPI not found");
+    }
+
+    public boolean isWarzone(Location location) {
+        throw new IllegalPluginAccessException("VaporAPI not found");
+    }
+
+    public boolean isWilderness(Location location) {
+        throw new IllegalPluginAccessException("VaporAPI not found");
+    }
+
+    public boolean isPlayerFaction(Player player) {
+        throw new IllegalPluginAccessException("VaporAPI not found");
+    }
+
+    public boolean isRoad(Location location) {
+        throw new IllegalPluginAccessException("VaporAPI not found");
+    }
+
+    public boolean isEvent(Location location) {
+        throw new IllegalPluginAccessException("VaporAPI not found");
+    }
+
+    public boolean isEndPortal(Location location) {
+        throw new IllegalPluginAccessException("VaporAPI not found");
+    }
+
+    public boolean isDTC(Location location) {
         throw new IllegalPluginAccessException("VaporAPI not found");
     }
 }
